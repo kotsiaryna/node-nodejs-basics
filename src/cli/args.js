287ac36eb5906b7f4c6beff1d6b800279b7671c6
Katsiaryna <katsiaryna.n@gmail.com>
2024-10-07 15:12:49 +0300

    feat: cli is done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2)
+    const parsedArgs = []
+    args.forEach((arg, i, arr) => {
+      if(arg.startsWith('--')) {
+        const key = arg.slice(2)
+        const value = arr[i+1]
+        parsedArgs.push(`${key} is ${value}`)
+      }
+ 
+    })
+
+    console.log(parsedArgs.join(', '))
+
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVars = process.env
+
+    const res = Object.keys(envVars).filter(variable => variable.startsWith('RSS_')).map(variable => `${variable}=${envVars[variable]}`).join('; ')
+    console.log(res)
 };
 
 parseEnv();
